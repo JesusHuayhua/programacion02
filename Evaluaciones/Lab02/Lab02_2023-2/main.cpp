9e2b526b085044eb0e3da13215e3f76274be3cfc
JesusHuayhua <jmhuayhua@pucp.edu.pe>
2024-04-09 23:20:02 -0500

    first part L2_23-2

---
+++ Evaluaciones/Lab02/Lab02_2023-2/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Jesus Huayhua Flores
+ * Codigo: 20196201
+ */
+
+#include "AsignacionDinamicaMemoriaExacta.h"
+
+int main(int argc, char** argv) {
+    char ***productos, ***codigoPedidos;
+    int *stock, *fechaPedidos, ***dniCantPedidos;
+    double *precios;
+    lecturaDeProductos("Productos.csv", productos, stock, precios);
+    pruebaDeLecturaDeProductos("ReporteDeProductos.txt", productos, stock, precios);
+    lecturaDePedidos("Pedidos.csv", fechaPedidos, codigoPedidos, dniCantPedidos);
+    //    pruebaDeLecturaDePedidos("ReporteDePedidos.txt", fechaPedidos, codigoPedidos, dniCantPedidos);
+    //    reporteDeEnvioDePedidos("ReporteDeEntregaDePedisos.txt", productos, stock, precios,
+    //            fechaPedidos, codigoPedidos, dniCantPedidos);
+    //    pruebaDeLecturaDeProductos("ReporteDeProductosFinal.txt", productos, stock, precios);
+    return 0;
+}
+
