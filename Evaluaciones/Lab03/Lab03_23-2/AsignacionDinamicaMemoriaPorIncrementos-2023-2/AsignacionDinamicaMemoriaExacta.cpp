e4a0b13df6e2583717e0e5a3239c34f946564b37
JesusHuayhua <jmhuayhua@pucp.edu.pe>
2024-04-17 15:29:00 -0500

    start lab03_23-2

---
+++ Evaluaciones/Lab03/Lab03_23-2/AsignacionDinamicaMemoriaPorIncrementos-2023-2/AsignacionDinamicaMemoriaExacta.cpp
@@ -0,0 +1,11 @@
+//
+// Created by errant on 17/04/24.
+//
+#include "AsignacionDinamicaMemoriaExacta.hpp"
+
+//char ***productos,***codigoPedidos;
+//int *stock, *fechaPedidos, ***dniCantPedidos;
+//double *precios;
+void lecturaDeProductos (const char *name,char ***&productos,int *& stock,double *& precios){
+
+}
